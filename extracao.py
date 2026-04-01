5fb9e19e0fbbe47f646b606c924aaa9e04ea229c
ChicoScience90 <lukasfo909090@gmail.com>
2026-04-01 15:20:24 -0300

    ajustes extracao

--- extracao.py
+++ extracao.py
@@ -2,15 +2,11 @@ from moviepy import VideoFileClip
 
 
 def extrair_audio(caminho_video, caminho_audio):
-    # Carrega o arquivo de vídeo
     video = VideoFileClip(r"C:\Users\lukas\OneDrive\Área de Trabalho\ssstwitter.com_1774873552091.mp4")
 
-    # Extrai o áudio e salva no formato desejado (mp3, wav, etc.)
     video.audio.write_audiofile(r"C:\Users\lukas\Downloads\hanzo.mp3")
 
-    # Fecha o arquivo para liberar memória
     video.close()
 
 
-# Exemplo de uso
 extrair_audio("seu_video.mp4", "resultado_audio.mp3")
