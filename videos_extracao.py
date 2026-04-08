10403d7dbe54faff84ce41cd96daa175fd558e2c
ChicoScience90 <lukasfo909090@gmail.com>
2026-04-08 10:57:49 -0300

    update_qualidade

--- videos_extracao.py
+++ videos_extracao.py
@@ -7,7 +7,13 @@ def extrair_video(url):
 
         'outtmpl': '%(title)s.%(ext)s',
 
-        'format': 'best',
+        # Baixa a melhor qualidade de vídeo e áudio separadamente e junta (se tiver ffmpeg)
+        # Tenta forçar mp4/m4a para melhor compatibilidade. Fallback para 'best' (720p).
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'merge_output_format': 'mp4',
+        
+        # Aponta pro ffmpeg que instalamos agora, pois o terminal não atualizou as variáveis
+        'ffmpeg_location': r'C:\Users\lukas\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.1-full_build\bin',
 
         'quiet': False,
         'no_warnings': True
@@ -25,6 +31,6 @@ def extrair_video(url):
         print(f"\nOcorreu um erro ao tentar baixar o vídeo: {e}")
 
 
-link_do_video = "https://www.youtube.com/watch?v=1ZGsugwZC2U"
+link_do_video = "https://www.youtube.com/watch?v=eAiVwsXgPDM"
 
 extrair_video(link_do_video)
